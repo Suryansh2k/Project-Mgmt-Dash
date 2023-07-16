4a3ebf283f2dc35135d9c40c0df69e7692c4ee18
Suryansh Sharma <suryanshsharmargh@gmail.com>
2023-07-16 13:11:27 +0530

    added all my files

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+      <nav className='navbar bg-light mb-4 p-0'>
+        <div className='container'>
+          <a className='navbar-brand' href='/'>
+            <div className='d-flex'>
+              <div>Freelance Project Dashboard</div>
+            </div>
+          </a>
+        </div>
+      </nav>
+    );
+  }
